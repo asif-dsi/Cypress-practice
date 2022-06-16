5c4a671a90130292630cec2e39356fd6fa55bcaa
asif-dsi <asif.shahriar@dsinnovators.com>
2022-06-16 11:10:30 +0600

    Updated test cases

--- cypress/integration/Pages/signup.test.js
+++ cypress/integration/Pages/signup.test.js
@@ -1,5 +1,3 @@
-require('cypress-xpath')
-
 describe("Sign up to Miro", () => {
     let info;
     beforeEach("Visit site", () => {
@@ -13,7 +11,6 @@ describe("Sign up to Miro", () => {
         Cypress.on('uncaught:exception', function (err, runnable) {
             return false
         })
-        cy.contains("Get started free today")
         cy.get('#name',{ timeout: 10000 }).should('be.visible').type(info.name)
         cy.get('#email',{ timeout: 10000 }).should('be.visible').type(info.email)
         cy.get('#password',{ timeout: 10000 }).should('be.visible').type(info.password)
@@ -22,6 +19,7 @@ describe("Sign up to Miro", () => {
         Cypress.on('uncaught:exception', function (err, runnable) {
             return false
         })
+        cy.get('#code',{ timeout: 10000 }).should('be.visible').type('1234')
         cy.contains('Check your email')
     })
 
@@ -30,7 +28,6 @@ describe("Sign up to Miro", () => {
         Cypress.on('uncaught:exception', function (err, runnable) {
             return false
         })
-        cy.contains("Get started free today")
         cy.get('#name',{ timeout: 10000 }).should('be.visible').type(info.name)
         cy.get('#email',{ timeout: 10000 }).should('be.visible').type(info.invalid_email)
         cy.get('#password',{ timeout: 10000 }).should('be.visible').click()
@@ -42,11 +39,55 @@ describe("Sign up to Miro", () => {
         Cypress.on('uncaught:exception', function (err, runnable) {
             return false
         })
-        cy.contains("Get started free today")
         cy.get('#name',{ timeout: 10000 }).should('be.visible').type(info.name)
         cy.get('#password',{ timeout: 10000 }).should('be.visible').type(info.password)
         cy.get('#signup-terms',{ timeout: 10000 }).should('be.visible').click({ force: true })
         cy.get('.signup__submit').click()
         cy.contains('Enter your email address.')
+    })
+
+    it("sign up with empty name", ()=> {
+        cy.visit('https://miro.com/signup/')
+        Cypress.on('uncaught:exception', function (err, runnable) {
+            return false
+        })
+        cy.get('#email',{ timeout: 10000 }).should('be.visible').type(info.email)
+        cy.get('#password',{ timeout: 10000 }).should('be.visible').type(info.password)
+        cy.get('#signup-terms',{ timeout: 10000 }).should('be.visible').click({ force: true })
+        cy.get('.signup__submit').click()
+        Cypress.on('uncaught:exception', function (err, runnable) {
+            return false
+        })
+        cy.contains('Please enter your name.')
+    })
+
+    it("sign up with empty password", ()=> {
+        cy.visit('https://miro.com/signup/')
+        Cypress.on('uncaught:exception', function (err, runnable) {
+            return false
+        })
+        cy.get('#name',{ timeout: 10000 }).should('be.visible').type(info.name)
+        cy.get('#email',{ timeout: 10000 }).should('be.visible').type(info.email)
+        cy.get('#signup-terms',{ timeout: 10000 }).should('be.visible').click({ force: true })
+        cy.get('.signup__submit').click()
+        Cypress.on('uncaught:exception', function (err, runnable) {
+            return false
+        })
+        cy.contains('Enter your password.')
+    })
+
+    it("sign up without accepting terms and condition", ()=> {
+        cy.visit('https://miro.com/signup/')
+        Cypress.on('uncaught:exception', function (err, runnable) {
+            return false
+        })
+        cy.get('#name',{ timeout: 10000 }).should('be.visible').type(info.name)
+        cy.get('#email',{ timeout: 10000 }).should('be.visible').type(info.email)
+        cy.get('#password',{ timeout: 10000 }).should('be.visible').type(info.password)
+        cy.get('.signup__submit').click()
+        Cypress.on('uncaught:exception', function (err, runnable) {
+            return false
+        })
+        cy.contains('Please agree with the Terms to sign up.')
     })
 })
